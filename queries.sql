b31cf436204a44903a9db143771fa4c9e1034658
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-07-22 22:47:03 -0400

    stretch for sql queries

--- queries.sql
+++ queries.sql
@@ -27,5 +27,10 @@ UPDATE Customers
 SET PostalCode = '11122'
 WHERE CustomerName = 'The Shire';
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT COUNT (DISTINCT CITY)
+FROM CUSTOMERS;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT *
+FROM Suppliers
+WHERE LENGTH(SupplierName) > 20;
