1bc1a51b43acb9b177fe06d6c4b031f1acc5f1a8
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-07-22 22:33:58 -0400

    added delete, post, put

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -14,4 +14,63 @@ router.get('/', async (req,res) => {
   }
 });
 
+router.get('/:id', async(req,res) => {
+  const { id } = req.params;
+
+  try{
+    const account = await db('accounts').where( { id }).first();
+    if(account) {
+      res.status(200).json(account);
+    } else {
+      res.status(404).json({ message: "account not found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post('/', async (req,res) => {
+  const accountData = req.body;
+
+  try {
+    const account = await db.insert(accountData).into('accounts');
+    res.status(201).json(account);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Unable to add account', error:err });
+  }
+});
+
+router.put('/:id', async (req,res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db('accounts').where({ id }).update(changes)
+    .then(count => {
+      if(count) {
+        res.status(200).json({ update: count });
+      } else {
+        res.status(404).json({ message: 'There is no account that matches that ID' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Unable to update account' });
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try{
+    const deleteAccount = await db.del().from('accounts').where({ id })
+    if(deleteAccount) {
+    res.status(200).json({ deleted: deleteAccount});
+  } else {
+    res.status(404).json ({ message: "There is no account with that ID" });
+  }
+} catch (err) {
+  res.status(500).json({ message: 'Database Error'});
+}
+});
+
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
 
 const AccountsRouter = require("../accounts/accounts-router.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(morgan('dev'));
 
 server.use("/api/accounts", AccountsRouter);
 
