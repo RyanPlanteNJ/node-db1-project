f7b6ce19a3dc1cd7557c71c87589410b04479d4d
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-07-22 21:46:35 -0400

    working on accounts

---
+++ accounts/accounts-router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', async (req,res) => {
+  try {
+    const accounts = await db('accounts');
+    res.json(accounts);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ message: "error retreiving accounts", err});
+  }
+});
